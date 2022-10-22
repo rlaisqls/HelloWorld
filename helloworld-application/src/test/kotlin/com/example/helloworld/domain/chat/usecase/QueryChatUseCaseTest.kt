96639ba4cc8397c60454c441da157beca493b004
rlaisqls <rlaisqls@gmail.com>
2022-10-22 22:47:59 +0900

    🧪 :: QueryChatUseCaseTest

---
+++ helloworld-application/src/test/kotlin/com/example/helloworld/domain/chat/usecase/QueryChatUseCaseTest.kt
@@ -0,0 +1,121 @@
+package com.example.helloworld.domain.chat.usecase
+
+import com.example.helloworld.domain.chat.spi.QueryChatPort
+import com.example.helloworld.domain.room.dto.response.QueryRoomListResponse
+import com.example.helloworld.domain.room.exception.RoomNotFoundException
+import com.example.helloworld.domain.room.exception.RoomParticipatingException
+import com.example.helloworld.domain.room.model.Room
+import com.example.helloworld.domain.room.spi.QueryRoomPort
+import com.example.helloworld.domain.room.spi.RoomUserPort
+import com.example.helloworld.domain.room.usecase.QueryMyRoomListUseCase
+import com.example.helloworld.domain.user.model.User
+import com.example.helloworld.domain.user.spi.UserPort
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito
+import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import java.time.LocalDateTime
+
+@ExtendWith(MockitoExtension::class)
+internal class QueryChatUseCaseTest {
+
+    @Mock
+    private lateinit var userPort: UserPort
+
+    @Mock
+    private lateinit var roomUserPort: RoomUserPort
+
+    @Mock
+    private lateinit var queryRoomPort: QueryRoomPort
+
+    @Mock
+    private lateinit var queryChatPort: QueryChatPort
+
+    @InjectMocks
+    private lateinit var queryChatUseCase: QueryChatUseCase
+
+    private val username = "rlaisqls"
+    private val password = "password"
+
+    private val userStub by lazy {
+        User(
+            username = username,
+            password = password
+        )
+    }
+
+    private val roomId = 1L
+    private val roomName = "roomName"
+    private val maxPeople = 5
+
+    private val roomStub by lazy {
+        Room(
+            id = roomId,
+            roomName = roomName,
+            maxPeople = maxPeople,
+            roomUsers = emptyList()
+        )
+    }
+
+    private val dateTime = LocalDateTime.of(2022,10,22,9,0)
+
+    @Test
+    fun 채팅_조회_성공() {
+        //given
+        given(userPort.getCurrentUser())
+            .willReturn(userStub)
+
+        given(queryRoomPort.queryRoomById(roomId))
+            .willReturn(roomStub)
+
+        given(roomUserPort.roomUserExists(roomStub, userStub))
+            .willReturn(true)
+
+        given(queryChatPort.queryChatByRoom(roomStub, dateTime))
+            .willReturn(emptyList())
+
+        //when
+        val response = queryChatUseCase.execute(roomId, dateTime)
+
+        //then
+        assertEquals(response.chatList, emptyList<QueryRoomListResponse>())
+    }
+
+    @Test
+    fun 채팅방을_찾을_수_없음() {
+        //given
+        given(userPort.getCurrentUser())
+            .willReturn(userStub)
+
+        given(queryRoomPort.queryRoomById(roomId))
+            .willReturn(null)
+
+        //when & then
+        assertThrows<RoomNotFoundException> {
+            queryChatUseCase.execute(roomId, dateTime)
+        }
+    }
+
+    @Test
+    fun 참여중이지_않은_방임() {
+        //given
+        given(userPort.getCurrentUser())
+            .willReturn(userStub)
+
+        given(queryRoomPort.queryRoomById(roomId))
+            .willReturn(roomStub)
+
+        given(roomUserPort.roomUserExists(roomStub, userStub))
+            .willReturn(false)
+
+        //when & then
+        assertThrows<RoomParticipatingException> {
+            queryChatUseCase.execute(roomId, dateTime)
+        }
+    }
+}
