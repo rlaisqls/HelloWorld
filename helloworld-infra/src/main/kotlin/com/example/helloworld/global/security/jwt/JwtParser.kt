763d7f64e0e34ee906350ce48d0755f50d98b09e
rlaisqls <rlaisqls@gmail.com>
2022-10-22 22:34:08 +0900

    🐛 ::  jwt 시크릿키 .toByteArray()

--- helloworld-infra/src/main/kotlin/com/example/helloworld/global/security/jwt/JwtParser.kt
+++ helloworld-infra/src/main/kotlin/com/example/helloworld/global/security/jwt/JwtParser.kt
@@ -4,10 +4,7 @@ import com.example.helloworld.global.security.SecurityProperties
 import com.example.helloworld.global.security.auth.AuthDetailsService
 import com.example.helloworld.global.security.exception.ExpiredTokenException
 import com.example.helloworld.global.security.exception.InvalidTokenException
-import io.jsonwebtoken.Claims
-import io.jsonwebtoken.Header
-import io.jsonwebtoken.Jws
-import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.*
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
@@ -17,13 +14,13 @@ import javax.servlet.http.HttpServletRequest
 class JwtParser(
     private val authDetailsService: AuthDetailsService,
     private val securityProperties: SecurityProperties
-){
+) {
 
     fun resolveToken(request: HttpServletRequest): String? {
 
         val bearerToken = request.getHeader(JwtProperty.HEADER)
 
-        if (bearerToken != null && (bearerToken.startsWith(JwtProperty.HEADER))) {
+        if (bearerToken != null && (bearerToken.startsWith(JwtProperty.PREFIX))) {
             return bearerToken.substring(7)
         }
         return null
@@ -31,8 +28,8 @@ class JwtParser(
 
     fun resolveToken(bearerToken: String?): String? {
 
-        if (bearerToken != null && (bearerToken.startsWith(JwtProperty.HEADER))) {
-            return bearerToken.substring(7)
+        if (bearerToken != null && (bearerToken.startsWith(JwtProperty.PREFIX))) {
+            return bearerToken.substring(JwtProperty.PREFIX.length + 1)
         }
         return null
     }
@@ -40,10 +37,9 @@ class JwtParser(
     fun getAuthentication(token: String?): Authentication {
 
         val claims = getClaims(token)
-        if (claims.header[Header.JWT_TYPE] == JwtProperty.REFRESH) {
+        if (claims.header[Header.JWT_TYPE] != JwtProperty.ACCESS) {
             throw InvalidTokenException.EXCEPTION
         }
-
         val details = authDetailsService.loadUserByUsername(claims.body.id)
         return UsernamePasswordAuthenticationToken(details, "", details.authorities)
     }
@@ -52,11 +48,11 @@ class JwtParser(
         return try {
             Jwts
                 .parser()
-                .setSigningKey(securityProperties.secretKey)
+                .setSigningKey(securityProperties.secretKey.toByteArray())
                 .parseClaimsJws(token)
         } catch (e: Exception) {
             when (e) {
-                is ExpiredTokenException -> throw ExpiredTokenException.EXCEPTION
+                is ExpiredJwtException -> throw ExpiredTokenException.EXCEPTION
                 else -> throw InvalidTokenException.EXCEPTION
             }
         }
