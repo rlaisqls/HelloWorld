e974b6b2d00b75b63d9dd588f374e1c3e30a1010
rlaisqls <rlaisqls@gmail.com>
2022-10-22 22:29:45 +0900

    ⚡️ :: 소켓 요청 어노테이션

--- helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/SocketChatWebAdapter.kt
+++ helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/SocketChatWebAdapter.kt
@@ -1,12 +1,12 @@
-package com.example.helloworld.chat
+package com.example.helloworld.domain.chat
 
 import com.example.helloworld.domain.chat.dto.request.SendChatRequest
-import com.example.helloworld.chat.dto.request.SendChatWebRequest
+import com.example.helloworld.domain.chat.dto.request.SendChatWebRequest
 import com.example.helloworld.domain.chat.usecase.SocketSendChatUseCase
+import com.example.helloworld.global.annotation.SocketEvent
+import com.example.helloworld.global.annotation.WebSocketAdapter
+import com.example.helloworld.global.property.SocketProperty
 import com.example.helloworld.global.socket.SocketClient
-import com.example.helloworld.socket.mapper.annotation.SocketEvent
-import com.example.helloworld.socket.mapper.annotation.WebSocketAdapter
-import com.example.helloworld.socket.property.SocketProperty
 import org.springframework.web.bind.annotation.RequestBody
 import javax.validation.Valid
 
@@ -17,7 +17,7 @@ class SocketChatWebAdapter (
 ) {
 
     @SocketEvent(SocketProperty.CHAT, SendChatWebRequest::class)
-    fun sendChat(socketClient: SocketClient, @Valid @RequestBody request: SendChatWebRequest) {
+    fun sendChat(socketClient: SocketClient, request: SendChatWebRequest) {
         sendChatUseCase.execute(
             socketClient = socketClient,
             request = SendChatRequest(
