cdc60fa84c9ac2527a8476d1d7ac4adf58317254
rlaisqls <rlaisqls@gmail.com>
2022-10-22 12:41:19 +0900

    📝 :: SocketSendChatUseCase

---
+++ helloworld-application/src/main/kotlin/com/example/helloworld/domain/chat/usecase/SocketSendChatUseCase.kt
@@ -0,0 +1,35 @@
+package com.example.helloworld.domain.chat.usecase
+
+import com.example.helloworld.domain.chat.dto.request.SendChatRequest
+import com.example.helloworld.domain.chat.model.Chat
+import com.example.helloworld.domain.chat.spi.CommandChatPort
+import com.example.helloworld.domain.chat.spi.SocketChatPort
+import com.example.helloworld.domain.chat.spi.SocketUserPort
+import com.example.helloworld.domain.user.spi.QueryUserPort
+import com.example.helloworld.global.annotation.UseCase
+import com.example.helloworld.global.socket.SocketClient
+import java.time.LocalDateTime
+
+@UseCase
+class SocketSendChatUseCase (
+    private val socketUserPort: SocketUserPort,
+    private val commandChatPort: CommandChatPort,
+    private val socketChatPort: SocketChatPort
+) {
+    fun execute(socketClient: SocketClient, request: SendChatRequest) {
+
+        val username = socketUserPort.getCurrentUsername(socketClient)
+        val roomId = socketUserPort.getCurrentRoomId(socketClient)
+
+        val chat = commandChatPort.save(
+            Chat(
+                roomId = roomId,
+                username = username,
+                message = request.message,
+                sentAt = LocalDateTime.now()
+            )
+        )
+
+        socketChatPort.sendChat(roomId, chat, username)
+    }
+}
