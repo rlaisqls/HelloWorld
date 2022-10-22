7d3026513909606dea1622337336142210c30976
rlaisqls <rlaisqls@gmail.com>
2022-10-22 22:09:07 +0900

    📝 :: ReissueTokenTokenUseCase

---
+++ helloworld-application/src/main/kotlin/com/example/helloworld/domain/auth/usecase/ReissueTokenUseCase.kt
@@ -0,0 +1,22 @@
+package com.example.helloworld.domain.auth.usecase
+
+import com.example.helloworld.domain.auth.dto.response.TokenResponse
+import com.example.helloworld.domain.auth.exception.RefreshTokenNotFoundException
+import com.example.helloworld.domain.auth.spi.JwtPort
+import com.example.helloworld.domain.auth.spi.QueryRefreshTokenPort
+import com.example.helloworld.global.annotation.UseCase
+
+@UseCase
+class ReissueTokenUseCase(
+    private val jwtPort: JwtPort,
+    private val queryRefreshTokenPort: QueryRefreshTokenPort
+) {
+    fun execute(request: String): TokenResponse {
+        val token = queryRefreshTokenPort.queryRefreshTokenByToken(request)
+            ?: throw RefreshTokenNotFoundException.EXCEPTION
+
+        return jwtPort.generateToken(
+             username = token.username
+        )
+    }
+}
