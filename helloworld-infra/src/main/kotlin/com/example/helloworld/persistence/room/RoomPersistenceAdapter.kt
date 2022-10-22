46c5feec47c935d3674f2180506cacb5e6369634
rlaisqls <rlaisqls@gmail.com>
2022-10-22 22:41:02 +0900

    ♻️ :: isFulledRoom

--- helloworld-infra/src/main/kotlin/com/example/helloworld/persistence/room/RoomPersistenceAdapter.kt
+++ helloworld-infra/src/main/kotlin/com/example/helloworld/persistence/room/RoomPersistenceAdapter.kt
@@ -1,19 +1,15 @@
 package com.example.helloworld.persistence.room
 
-import com.example.helloworld.domain.room.exception.RoomNotFoundException
+import com.example.helloworld.domain.room.exception.FulledRoomException
 import com.example.helloworld.domain.room.model.Room
 import com.example.helloworld.domain.room.model.RoomUser
 import com.example.helloworld.domain.room.spi.RoomPort
 import com.example.helloworld.domain.user.model.User
 import com.example.helloworld.persistence.room.mapper.RoomMapper
 import com.example.helloworld.persistence.room.mapper.RoomUserMapper
-import com.example.helloworld.persistence.room.model.RoomJpaEntity
-import com.example.helloworld.persistence.room.model.RoomUserJpaEntity
-import com.example.helloworld.persistence.room.model.RoomUserJpaId
 import com.example.helloworld.persistence.room.repository.RoomJpaRepository
 import com.example.helloworld.persistence.room.repository.RoomUserJpaRepository
 import com.example.helloworld.persistence.user.mapper.UserMapper
-import com.example.helloworld.persistence.user.repository.UserJpaRepository
 import org.springframework.stereotype.Component
 
 @Component
@@ -21,39 +17,47 @@ class RoomPersistenceAdapter(
     private val roomRepository: RoomJpaRepository,
     private val roomUserRepository: RoomUserJpaRepository,
     private val roomMapper: RoomMapper,
+    private val roomUserMapper: RoomUserMapper,
     private val userMapper: UserMapper
 ) : RoomPort {
 
-    override fun save(room: Room) = roomMapper.toDomain(
+    override fun saveRoom(room: Room) = roomMapper.toDomain(
         roomRepository.save(
             roomMapper.toEntity(room)
         )
     )!!
 
     override fun roomUserExists(room: Room, user: User): Boolean {
-        return roomUserRepository
-            .existsById(getRoomUserId(room, user))
+
+        val roomEntity = roomMapper.toEntity(room)
+        val userEntity = userMapper.toEntity(user)
+
+        return roomUserRepository.existsByRoomAndUser(roomEntity, userEntity)
+    }
+
+    override fun isFulledRoom(room: Room): Boolean {
+        val roomEntity = roomRepository.queryRoomById(room.id)!!
+        return roomEntity.roomUsers.size == room.maxPeople
     }
 
     override fun addRoomUser(room: Room, user: User) {
+
+        val roomUser = RoomUser(
+            roomId = room.id,
+            userId = user.id
+        )
+
         roomUserRepository.save(
-            RoomUserJpaEntity(getRoomUserId(room, user))
+            roomUserMapper.toEntity(roomUser)
         )
     }
 
-    private fun getRoomUserId(room: Room, user: User) = RoomUserJpaId(
-        room = room.id,
-        user = user.id
-    )
-
     override fun deleteRoomUser(room: Room, user: User) {
 
-        val roomUser = roomUserRepository.queryByRoomAndUser(
-            room = roomMapper.toEntity(room),
-            user = userMapper.toEntity(user)
-        )
+        val roomEntity = roomMapper.toEntity(room)
 
-        roomUserRepository.delete(roomUser)
+        roomEntity.roomUsers.removeIf { it.user.id == user.id }
+        roomRepository.save(roomEntity)
     }
 
     override fun queryRoomById(roomId: Long): Room? = roomMapper.toDomain(
@@ -61,7 +65,10 @@ class RoomPersistenceAdapter(
     )
 
     override fun queryMyRoomList(user: User): List<Room> {
-        return roomUserRepository.queryByUser(userMapper.toEntity(user))
+
+        val userEntity = userMapper.toEntity(user)
+
+        return roomUserRepository.queryByUser(userEntity)
             .map { roomMapper.toDomain(it.room)!! }
     }
 
