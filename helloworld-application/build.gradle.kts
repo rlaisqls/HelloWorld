046fc4ae0d330bef1d373440a0b4fa68b8c01388
rlaisqls <rlaisqls@gmail.com>
2022-10-25 22:21:22 +0900

    ⚙️ :: Test liblary 서브모듈로 이동

--- helloworld-application/build.gradle.kts
+++ helloworld-application/build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    id("org.springframework.boot") version "2.7.4"
-    id("io.spring.dependency-management") version "1.0.14.RELEASE"
     kotlin("plugin.allopen") version "1.6.21"
 }
 
@@ -9,6 +7,9 @@ dependencies {
 
     implementation("org.springframework:spring-context:5.3.23")
     implementation("org.springframework:spring-tx:5.3.23")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.5")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
 
 allOpen {
