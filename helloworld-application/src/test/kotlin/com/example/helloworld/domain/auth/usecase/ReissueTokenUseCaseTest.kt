b705d19959cd46ea1a9c09445c097ca579cd1644
rlaisqls <rlaisqls@gmail.com>
2022-10-25 18:43:06 +0900

    🧪 :: ReissueTokenUseCaseTest

---
+++ helloworld-application/src/test/kotlin/com/example/helloworld/domain/auth/usecase/ReissueTokenUseCaseTest.kt
@@ -0,0 +1,78 @@
+package com.example.helloworld.domain.auth.usecase
+
+import com.example.helloworld.domain.auth.dto.response.TokenResponse
+import com.example.helloworld.domain.auth.exception.RefreshTokenNotFoundException
+import com.example.helloworld.domain.auth.model.RefreshToken
+import com.example.helloworld.domain.auth.spi.QueryRefreshTokenPort
+import com.example.helloworld.domain.room.exception.RoomParticipatingException
+import com.example.helloworld.domain.user.dto.request.ChangePasswordRequest
+import com.example.helloworld.domain.user.spi.UserJwtPort
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.then
+import java.time.LocalDateTime
+
+@ExtendWith(MockitoExtension::class)
+internal class ReissueTokenUseCaseTest {
+
+    @Mock
+    private lateinit var userJwtPort: UserJwtPort
+
+    @Mock
+    private lateinit var queryRefreshTokenPort: QueryRefreshTokenPort
+
+    @InjectMocks
+    private lateinit var reissueTokenUseCase: ReissueTokenUseCase
+
+    private val username = "rlaisqls"
+    private val refreshToken = "refreshToken"
+
+    private val refreshTokenStub by lazy {
+        RefreshToken(
+            token = refreshToken,
+            username = username,
+            expiration = 123456789
+        )
+    }
+
+    private val tokenResponseStub by lazy {
+        TokenResponse(
+            accessToken = "accessToken",
+            accessTokenExp = LocalDateTime.now(),
+            refreshToken = "accessToken"
+        )
+    }
+
+    @Test
+    fun 토큰_재발급_성공() {
+        //given
+        given(queryRefreshTokenPort.queryRefreshTokenByToken(refreshToken))
+            .willReturn(refreshTokenStub)
+
+        //when
+        reissueTokenUseCase.execute(refreshToken)
+
+        //then
+        then(userJwtPort).should(times(1)).updateToken(any(), any())
+    }
+
+    @Test
+    fun refresh_token_존재하지_않음() {
+        //given
+        given(queryRefreshTokenPort.queryRefreshTokenByToken(refreshToken))
+            .willReturn(null)
+
+        //when & then
+        assertThrows<RefreshTokenNotFoundException> {
+            reissueTokenUseCase.execute(refreshToken)
+        }
+    }
+}
