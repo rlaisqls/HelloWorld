995ee28842f01afd56517b161db68a251a85f9fc
rlaisqls <rlaisqls@gmail.com>
2022-10-25 18:40:18 +0900

    ⚡️ :: TokenReissue 방식 변경

--- helloworld-application/src/main/kotlin/com/example/helloworld/domain/auth/usecase/ReissueTokenUseCase.kt
+++ helloworld-application/src/main/kotlin/com/example/helloworld/domain/auth/usecase/ReissueTokenUseCase.kt
@@ -2,21 +2,23 @@ package com.example.helloworld.domain.auth.usecase
 
 import com.example.helloworld.domain.auth.dto.response.TokenResponse
 import com.example.helloworld.domain.auth.exception.RefreshTokenNotFoundException
-import com.example.helloworld.domain.auth.spi.JwtPort
 import com.example.helloworld.domain.auth.spi.QueryRefreshTokenPort
+import com.example.helloworld.domain.user.spi.UserJwtPort
 import com.example.helloworld.global.annotation.UseCase
+import org.springframework.stereotype.Service
 
 @UseCase
 class ReissueTokenUseCase(
-    private val jwtPort: JwtPort,
+    private val userJwtPort: UserJwtPort,
     private val queryRefreshTokenPort: QueryRefreshTokenPort
 ) {
     fun execute(request: String): TokenResponse {
         val token = queryRefreshTokenPort.queryRefreshTokenByToken(request)
             ?: throw RefreshTokenNotFoundException.EXCEPTION
 
-        return jwtPort.generateToken(
-             username = token.username
+        return userJwtPort.updateToken(
+            refreshToken = token.token,
+            username = token.username
         )
     }
 }
