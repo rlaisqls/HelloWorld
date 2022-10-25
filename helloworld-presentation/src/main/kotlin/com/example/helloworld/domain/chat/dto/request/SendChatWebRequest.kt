eaea2ccf716789640efe98b089a6dd27f57fdbfd
rlaisqls <rlaisqls@gmail.com>
2022-10-25 18:35:40 +0900

    ⚡️ :: @NotNull validation

--- helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/dto/request/SendChatWebRequest.kt
+++ helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/dto/request/SendChatWebRequest.kt
@@ -1,9 +1,12 @@
 package com.example.helloworld.domain.chat.dto.request
 
 import java.beans.ConstructorProperties
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class SendChatWebRequest @ConstructorProperties("message") constructor(
+    
+    @field:NotNull(message = "message는 null이어선 안됩니다")
     @field:Size(max = 255, message = "room_name은 15자 이하여야합니다.")
     val message: String
 )
