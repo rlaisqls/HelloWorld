ed5f29152d9061f397d852a3f883b822eb278600
rlaisqls <rlaisqls@gmail.com>
2022-11-18 20:19:14 +0900

    Bug: 오타

--- helloworld-domain/src/main/kotlin/com/example/helloworld/domain/file/exception/FilePathNotFoundException.kt
+++ helloworld-domain/src/main/kotlin/com/example/helloworld/domain/file/exception/FilePathNotFoundException.kt
@@ -3,4 +3,4 @@ package com.example.helloworld.domain.file.exception
 import com.example.helloworld.domain.file.error.FileErrorCode
 import com.example.helloworld.global.error.BusinessException
 
-object FilPathNotFoundException : BusinessException(FileErrorCode.FILE_PATH_NOT_FOUND)
+object FilePathNotFoundException : BusinessException(FileErrorCode.FILE_PATH_NOT_FOUND)
