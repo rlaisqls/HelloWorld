8b8e7fcfc64fb47708bb260a4b69ae9cfa56701d
rlaisqls <rlaisqls@gmail.com>
2022-11-18 20:15:28 +0900

    Refactoring: 가독성 개선

--- helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/ChatWebAdapter.kt
+++ helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/ChatWebAdapter.kt
@@ -1,20 +1,20 @@
 package com.example.helloworld.domain.chat
 
-import com.example.helloworld.domain.chat.dto.request.SendChatWebRequest
 import com.example.helloworld.domain.chat.dto.response.QueryChatListResponse
 import com.example.helloworld.domain.chat.usecase.QueryChatUseCase
 import com.example.helloworld.global.annotation.WebAdapter
 import org.springframework.format.annotation.DateTimeFormat
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import java.time.LocalDateTime
-import javax.validation.Valid
 
 @WebAdapter
 @RequestMapping("/chats")
 class ChatWebAdapter (
     private val queryChatUseCase: QueryChatUseCase
 ) {
-
     @GetMapping("/room/{room-id}")
     fun queryChat(@PathVariable("room-id") roomId: Long,
                   @RequestParam
--- helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/dto/request/SendChatWebRequest.kt
+++ helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/chat/dto/request/SendChatWebRequest.kt
@@ -6,7 +6,7 @@ import javax.validation.constraints.Size
 
 data class SendChatWebRequest @ConstructorProperties("message") constructor(
     
-    @field:NotNull(message = "message는 null이어선 안됩니다")
-    @field:Size(max = 255, message = "room_name은 15자 이하여야합니다.")
+    @field:NotNull
+    @field:Size(max = 255)
     val message: String
 )
