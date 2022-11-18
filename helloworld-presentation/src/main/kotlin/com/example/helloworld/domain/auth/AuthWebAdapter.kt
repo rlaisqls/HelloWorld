a35758829481ee779823d54468932d0dfc05089a
rlaisqls <rlaisqls@gmail.com>
2022-11-18 20:20:53 +0900

    Create: AuthWebAdapter

--- helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/auth/AuthWebAdapter.kt
+++ helloworld-presentation/src/main/kotlin/com/example/helloworld/domain/auth/AuthWebAdapter.kt
@@ -1,22 +1,35 @@
 package com.example.helloworld.domain.auth
 
+import com.example.helloworld.domain.auth.dto.response.AuthLinkResponse
 import com.example.helloworld.domain.auth.dto.response.TokenResponse
+import com.example.helloworld.domain.auth.spi.dto.request.CodeRequest
+import com.example.helloworld.domain.auth.usecase.GoogleAuthUseCase
+import com.example.helloworld.domain.auth.usecase.QueryGoogleAuthLinkUseCase
 import com.example.helloworld.domain.auth.usecase.ReissueTokenUseCase
 import com.example.helloworld.global.annotation.WebAdapter
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @WebAdapter
 @RequestMapping("/users")
 class AuthWebAdapter(
-    private val reissueTokenUseCase: ReissueTokenUseCase
+    private val reissueTokenUseCase: ReissueTokenUseCase,
+    private val queryGoogleAuthLinkUseCase: QueryGoogleAuthLinkUseCase,
+    private val googleAuthUseCase: GoogleAuthUseCase
 ) {
-
     @PutMapping("/auth")
     fun tokenRefresh(@RequestHeader("X-Refresh-Token") refreshToken: String): TokenResponse {
         return reissueTokenUseCase.execute(refreshToken)
     }
+
+    @GetMapping("/google")
+    fun queryGoogleAuthLink(): AuthLinkResponse {
+        return queryGoogleAuthLinkUseCase.execute()
+    }
+
+    @PostMapping("/google")
+    fun googleAuthInfo(@ModelAttribute request: CodeRequest): TokenResponse {
+        return googleAuthUseCase.execute(request)
+    }
+
 }
