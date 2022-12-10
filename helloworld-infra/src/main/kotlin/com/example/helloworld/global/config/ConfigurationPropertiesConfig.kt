0c071dce7b56231fd0d0b21da22590450c4b94b1
rlaisqls <rlaisqls@gmail.com>
2022-12-10 13:35:32 +0900

    Update: ConfigurationPropertiesScan 분리

---
+++ helloworld-infra/src/main/kotlin/com/example/helloworld/global/config/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package com.example.helloworld.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan("com.example.helloworld")
+@Configuration
+class ConfigurationPropertiesConfig
