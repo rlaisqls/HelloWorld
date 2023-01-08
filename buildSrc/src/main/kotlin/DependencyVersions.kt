c13551c80c19bc2d822860e1a03464af3206f743
rlaisqls <rlaisqls@gmail.com>
2023-01-08 15:10:58 +0900

    create: DependencyVersions

---
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,7 @@
+object DependencyVersions {
+    const val JACKSON_VERSION = "2.13.4"
+    const val KOTEST_VERSION = "5.5.4"
+    const val MOCKK_VERSION = "1.13.2"
+    const val QUERYDSL_VERSION = "5.0.0"
+    const val SOCKET_IO_VERSION = "1.7.22"
+}
